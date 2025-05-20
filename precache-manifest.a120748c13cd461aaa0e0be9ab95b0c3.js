cb7e6045152bb35c021c7bfd32abbba2e63f6c1e
Sara Hanlon <sarasgeniusmachine@Saras-MBP-2.lan>
2025-05-20 17:37:11 -0400

    Updates

---
+++ precache-manifest.a120748c13cd461aaa0e0be9ab95b0c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d5dc3f445492060b5ff9ca85d3b679",
+    "url": "/TheRealPrivilege/index.html"
+  },
+  {
+    "revision": "be93065be1cc31e86b2f",
+    "url": "/TheRealPrivilege/static/css/main.9ad3fa2a.chunk.css"
+  },
+  {
+    "revision": "905e68bf27c4c53db5ac",
+    "url": "/TheRealPrivilege/static/js/2.9731ee82.chunk.js"
+  },
+  {
+    "revision": "1eb98706859bae847c8762bbc3c10000",
+    "url": "/TheRealPrivilege/static/js/2.9731ee82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402dc92077224e63aaf3",
+    "url": "/TheRealPrivilege/static/js/3.d8d546e1.chunk.js"
+  },
+  {
+    "revision": "be93065be1cc31e86b2f",
+    "url": "/TheRealPrivilege/static/js/main.16a0009d.chunk.js"
+  },
+  {
+    "revision": "c7d7ba210ac02641251a",
+    "url": "/TheRealPrivilege/static/js/runtime-main.f395baab.js"
+  }
+]);
